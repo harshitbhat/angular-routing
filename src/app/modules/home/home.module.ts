d383a8d4cdd01b41e64133da009089096ba48ef5
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-11-01 11:25:44 +0530

    Import and Export Module

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { HomeMainComponent } from './components/home-main/home-main.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeMainComponent],
   imports: [CommonModule, HomeRoutingModule],
+  exports: [HomeMainComponent],
 })
 export class HomeModule {}
