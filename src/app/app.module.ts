d97809cc2604f6612efa57bb4382cc1e2973e110
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-11-01 11:40:49 +0530

    Add 2 modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HomeModule } from './modules/home/home.module';
+import { ContactModule } from './modules/contact/contact.module';
+import { AboutModule } from './modules/about/about.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HomeModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule,
+    ContactModule,
+    AboutModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/modules/about/about-routing.module.ts
+++ src/app/modules/about/about-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMainComponent } from './components/about-main/about-main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'about',
+    component: AboutMainComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AboutRoutingModule { }
+export class AboutRoutingModule {}
